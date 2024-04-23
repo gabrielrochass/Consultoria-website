dde3d9c9071c5bf90e54b2670325eae6360f52ef
Gabriel Rocha <120466775+gabrielrochass@users.noreply.github.com>
2024-04-22 23:01:45 -0300

    feat: serviços

--- main.js
+++ main.js
@@ -1,52 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const slides = document.querySelectorAll('.slide');
-    const dots = document.querySelectorAll('.dot');
-    let currentSlide = 0;
-
-    function showSlide(index) {
-        slides.forEach((slide) => {
-            slide.style.opacity = 0;
-        });
-        slides[index].style.opacity = 1;
-
-        dots.forEach((dot) => {
-            dot.classList.remove('active');
-        });
-        dots[index].classList.add('active');
-    }
-
-    function nextSlide() {
-        currentSlide = (currentSlide + 1) % slides.length;
-        showSlide(currentSlide);
-    }
-
-    dots.forEach((dot, index) => {
-        dot.addEventListener('click', () => {
-            currentSlide = index;
-            showSlide(currentSlide);
-        });
-    });
-
-    setInterval(nextSlide, 5000); // Troca de slide a cada 5 segundos
-});
-
-var swiper = new Swiper('.swiper-container', {
-    effect: 'coverflow',
+var swiper = new Swiper(".swiper", {
+    effect: "coverflow",
     grabCursor: true,
     centeredSlides: true,
-    slidesPerView: 'auto',
+    slidesPerView: 3,
+    speed: 600,
     coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
     },
-    pagination: {
-        el: '.swiper-pagination',
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-});
+    loop: true,
+  });
+  
