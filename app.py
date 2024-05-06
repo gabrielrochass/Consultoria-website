0745006e432603a3054a4da3ae64b906a819001a
Gabriel Rocha <120466775+gabrielrochass@users.noreply.github.com>
2024-05-06 13:53:08 -0300

    feat: enviar curriculo

--- app.py
+++ app.py
@@ -1,35 +1,58 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
+import os
+from werkzeug.utils import secure_filename
 from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
-# Configurações do Flask-Mail
+# Configurações para o Flask-Mail
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = 'gabrielrsilva203@gmail.com'  # Insira seu e-mail aqui
-app.config['MAIL_PASSWORD'] = 'Rochagabriel2003'  # Insira sua senha aqui
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'seu_email@gmail.com'  # Seu email aqui
+app.config['MAIL_PASSWORD'] = 'sua_senha'  # Sua senha aqui
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 
 mail = Mail(app)
+
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/submit_form', methods=['POST'])
-def submit_form():
-    if request.method == 'POST':
-        nome = request.form['nome']
-        email = request.form['email']
-        mensagem = request.form['mensagem']
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'Nenhum arquivo enviado'
+    
+    file = request.files['file']
+    
+    if file.filename == '':
+        return 'Nome do arquivo vazio'
+    
+    if file:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        
+        # Verifica qual botão foi clicado
+        submit_button = request.form['submit']
+        if submit_button == 'curriculo':
+            send_email('smterceirizacaoeservicos@gmail.com', filename)
+        elif submit_button == 'proposta':
+            send_email('rhsmterceirizacaoeservicos@gmail.com', filename)
+        
+        return 'Arquivo enviado com sucesso'
 
-        # Enviar e-mail
-        msg = Message('Nova mensagem de contato', sender='gabrielrsilva203@gmail.com', recipients=['smterceirizacaoeservicos@gmail.com'])
-        msg.body = f"Nome: {nome}\nE-mail: {email}\nMensagem: {mensagem}"
-        mail.send(msg)
-
-        return 'Mensagem enviada com sucesso!'
+def send_email(recipient, filename):
+    msg = Message('Novo arquivo recebido',
+                  sender='seu_email@gmail.com',
+                  recipients=[recipient])
+    msg.body = 'Um novo arquivo foi enviado.'
+    with app.open_resource(os.path.join(app.config['UPLOAD_FOLDER'], filename)) as fp:
+        msg.attach(filename, 'application/octet-stream', fp.read())
+    mail.send(msg)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
