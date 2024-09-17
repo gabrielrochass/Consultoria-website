4da8fc8d0ab736f2c109dc7fa072cac3ed0547e3
gabrielrochass <120466775+gabrielrochass@users.noreply.github.com>
2024-09-17 18:48:47 -0300

    fix: 5 cards

--- meu-projeto/src/componentes/valores/valores.jsx
+++ meu-projeto/src/componentes/valores/valores.jsx
@@ -7,7 +7,7 @@ import './valores.css';
 const Valores = () => {
 return (
     <div id="valores">
-        <h1>Nossos Valores</h1>
+        <h1>Nossos Ideais</h1>
         <div className="mural">
             <div className="valor">
                 <div className="emoji"> <SiTrueup /> </div>
@@ -19,6 +19,18 @@ return (
                 <div className="emoji"> <GiBrain /> </div>
                 <h2>Inovação</h2>
                 <p>Estamos constantemente buscando maneiras criativas de melhorar e inovar. Abraçamos novas ideias e desafiamos o status quo para impulsionar o progresso.</p>
+            </div>
+
+            <div className="valor">
+                <div className="emoji"><FaHandshakeSimple /></div>
+                <h2>Colaboração</h2>
+                <p>Acreditamos no poder da colaboração e do trabalho em equipe. Valorizamos a diversidade de pensamento e trabalhamos juntos para alcançar objetivos comuns.</p>
+            </div>
+
+            <div className="valor">
+                <div className="emoji"><FaHandshakeSimple /></div>
+                <h2>Colaboração</h2>
+                <p>Acreditamos no poder da colaboração e do trabalho em equipe. Valorizamos a diversidade de pensamento e trabalhamos juntos para alcançar objetivos comuns.</p>
             </div>
 
             <div className="valor">
