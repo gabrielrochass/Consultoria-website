728714ee86fde9447335fe2eb631e07199fcff43
gabrielrochass <120466775+gabrielrochass@users.noreply.github.com>
2024-12-06 17:16:24 -0300

    fix: navbar v1

--- meu-projeto/src/componentes/navbar/navbar.js
+++ meu-projeto/src/componentes/navbar/navbar.js
@@ -1,22 +1,28 @@
-// navbar para navegação entre páginas
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './navbar.css'
+// Navbar para navegação entre páginas
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './navbar.css';
 
 export default function Navbar() {
     return (
         <nav>
-            <div className="logo">
-                <Link to="/">
-                    <img src="../assets/logo.png" alt="Logo" />
-                </Link>
+            <div className="navbar-left">
+                <div className="logo">
+                    <Link to="/">
+                        <img src={require('../../assets/logo.png')} alt="Logo" />
+                    </Link>
+                </div>
+                <div className="menu-nome">
+                    <h1>SM Terceirização e Serviços</h1>
+                </div>
             </div>
+
             <ul className="menu">
-                <li><Link to="#home">Home</Link></li>
-                <li><Link to="#sobre">Sobre</Link></li>
-                <li><Link to="#servicos">Serviços</Link></li>
-                <li><Link to="#contato">Contato</Link></li>
-            </ul>      
+                <li><Link to="home">Home</Link></li>
+                <li><Link to="sobre">Sobre</Link></li>
+                <li><Link to="servicos">Serviços</Link></li>
+                <li><Link to="contato">Contato</Link></li>
+            </ul>
         </nav>
-    )
+    );
 }
