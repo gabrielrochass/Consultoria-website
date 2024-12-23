2714b0440d61b831880a2f8da0e2545c3429d467
gabrielrochass <grss@cin.ufpe.br>
2024-12-23 18:54:10 -0300

    feat: mission v1

--- meu-projeto/src/componentes/valores/valores.jsx
+++ meu-projeto/src/componentes/valores/valores.jsx
@@ -1,44 +1,25 @@
 import React from 'react';
-import { FaHandshakeSimple } from "react-icons/fa6";
-import { GiBrain } from "react-icons/gi";
-import { SiTrueup } from "react-icons/si";
 import './valores.css';
 
 const Valores = () => {
 return (
     <div id="valores">
-        <h1>Nossos Ideais</h1>
-        <div className="mural">
-            <div className="valor">
-                <div className="emoji"> <SiTrueup /> </div>
-                <h2>Integridade</h2>
-                <p>Nosso compromisso com a integridade guia todas as nossas ações. Mantemos altos padrões éticos e agimos com honestidade e transparência em tudo o que fazemos.</p>
-            </div>
-
-            <div className="valor">
-                <div className="emoji"> <GiBrain /> </div>
-                <h2>Inovação</h2>
-                <p>Estamos constantemente buscando maneiras criativas de melhorar e inovar. Abraçamos novas ideias e desafiamos o status quo para impulsionar o progresso.</p>
-            </div>
-
-            <div className="valor">
-                <div className="emoji"><FaHandshakeSimple /></div>
-                <h2>Colaboração</h2>
-                <p>Acreditamos no poder da colaboração e do trabalho em equipe. Valorizamos a diversidade de pensamento e trabalhamos juntos para alcançar objetivos comuns.</p>
-            </div>
-
-            <div className="valor">
-                <div className="emoji"><FaHandshakeSimple /></div>
-                <h2>Colaboração</h2>
-                <p>Acreditamos no poder da colaboração e do trabalho em equipe. Valorizamos a diversidade de pensamento e trabalhamos juntos para alcançar objetivos comuns.</p>
-            </div>
-
-            <div className="valor">
-                <div className="emoji"><FaHandshakeSimple /></div>
-                <h2>Colaboração</h2>
-                <p>Acreditamos no poder da colaboração e do trabalho em equipe. Valorizamos a diversidade de pensamento e trabalhamos juntos para alcançar objetivos comuns.</p>
-            </div>
+        <h1>Nossos Valores</h1>
+        <div className="missao">
+            <h2>MISSÃO</h2>
+            <p>Fornecer soluções de terceirização e serviços de alta qualidade, atendendo às necessidades específicas de nossos clientes e contribuindo para o seu sucesso.</p>
         </div>
+
+        <div className="visao">
+            <h2>VISÃO</h2>
+            <p>Ser reconhecida como a principal empresa de terceirização de serviços no Brasil, destacando-se pela inovação, qualidade e compromisso com a satisfação dos clientes. Almejamos expandir nossa atuação para novos mercados, sempre mantendo a ética e a responsabilidade social como pilares fundamentais de nosso crescimento.</p>
+        </div>
+
+        <div className="valores">
+            <h2>VALORES</h2>
+            <p>Na SM Terceirização e Serviços Ltda, valorizamos a excelência nos serviços, a resiliência frente aos desafios e o compromisso com a responsabilidade social e ambiental.</p>
+        </div>
+        
     </div>
 )
 }
