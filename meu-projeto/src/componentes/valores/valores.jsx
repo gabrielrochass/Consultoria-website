221420f8166ddf1f2dca0df436c76873d65e4d79
gabrielrochass <grss@cin.ufpe.br>
2024-12-30 14:56:34 -0300

    fix: visão section melhorada

--- meu-projeto/src/componentes/valores/valores.jsx
+++ meu-projeto/src/componentes/valores/valores.jsx
@@ -1,27 +1,51 @@
 import React from 'react';
+import { GiEyeTarget } from "react-icons/gi";
+import { IoDiamondOutline, IoRocketOutline } from "react-icons/io5";
 import './valores.css';
 
 const Valores = () => {
-return (
-    <div id="valores">
-        <h1>Nossos Valores</h1>
-        <div className="missao">
-            <h2>MISSÃO</h2>
-            <p>Fornecer soluções de terceirização e serviços de alta qualidade, atendendo às necessidades específicas de nossos clientes e contribuindo para o seu sucesso.</p>
-        </div>
 
-        <div className="visao">
-            <h2>VISÃO</h2>
-            <p>Ser reconhecida como a principal empresa de terceirização de serviços no Brasil, destacando-se pela inovação, qualidade e compromisso com a satisfação dos clientes. Almejamos expandir nossa atuação para novos mercados, sempre mantendo a ética e a responsabilidade social como pilares fundamentais de nosso crescimento.</p>
+    return (
+        <div id="valores" className="valores-container">
+            <h1 className="valores-title">MISSÃO, VISÃO E VALORES</h1>
+            <p className="valores-description">
+                Fornecemos soluções de alta qualidade para atender às suas necessidades, com inovação e compromisso social.
+            </p>
+            <div className="valores-content">
+                <div className="valores-circles">
+                    <div className="circle missoes-circle">
+                        <IoRocketOutline />
+                    </div>
+                    <div className="circle visao-circle">
+                        <GiEyeTarget />
+                    </div>
+                    <div className="circle valores-circle">
+                        <IoDiamondOutline />
+                    </div>
+                </div>
+                <div className="valores-text">
+                    <div className="text-section missoes-text">
+                        <h2>MISSÃO</h2>
+                        <p>
+                            Fornecer soluções de terceirização e serviços de alta qualidade, atendendo às necessidades específicas de nossos clientes e contribuindo para o seu sucesso.
+                        </p>
+                    </div>
+                    <div className="text-section visao-text">
+                        <h2>VISÃO</h2>
+                        <p>
+                            Ser reconhecida como a principal empresa de terceirização de serviços no Brasil, destacando-se pela inovação, qualidade e compromisso com a satisfação dos clientes.
+                        </p>
+                    </div>
+                    <div className="text-section valores-text">
+                        <h2>VALORES</h2>
+                        <p>
+                            Valorizamos a excelência nos serviços, a resiliência frente aos desafios e o compromisso com a responsabilidade social e ambiental.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
+    );
+};
 
-        <div className="valores">
-            <h2>VALORES</h2>
-            <p>Na SM Terceirização e Serviços Ltda, valorizamos a excelência nos serviços, a resiliência frente aos desafios e o compromisso com a responsabilidade social e ambiental.</p>
-        </div>
-        
-    </div>
-)
-}
-
-export default Valores
+export default Valores;
