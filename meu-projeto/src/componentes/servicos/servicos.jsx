90c215c657071e971e8028f5a2955923088704e1
gabrielrochass <grss@cin.ufpe.br>
2025-01-09 00:48:48 -0300

    fix: servicos completo

--- meu-projeto/src/componentes/servicos/servicos.jsx
+++ meu-projeto/src/componentes/servicos/servicos.jsx
@@ -8,20 +8,20 @@ const Servicos = () => {
             <div className="container">
                 <div className="custom-card">
                     <div className="img-box">
-                        <img src={require('../../assets/solange.png')} alt="Consultoria em gestão empresarial" />
+                        <img src={require('../../assets/solange3.png')} alt="Consultoria em gestão empresarial" />
                     </div>
                     <div className="custom-content">
-                        <h2>Consultoria Empresarial</h2>
-                        <p>Oferecemos consultoria especializada em gestão empresarial para melhorar a eficiência e a rentabilidade do seu negócio.</p>
+                        <h2>Consultoria em Gestão de Contratos</h2>
+                        <p>Assessoria especializada na gestão de contratos, incluindo elaboração, controle, repactuação, reajuste e renovação, otimizando os resultados para sua empresa.</p>
                     </div>
                 </div>
                 <div className="custom-card">
                     <div className="img-box">
-                    <img src={require('../../assets/limpezaaa.png')} alt="Limpeza e Conservação" />
+                    <img src={require('../../assets/conserv.png')} alt="Limpeza e Conservação" />
                     </div>
                     <div className="custom-content">
-                        <h2>Limpeza e Conservação</h2>
-                        <p>Oferecemos serviços de limpeza predial e hospitalar, incluindo limpeza pós-obra para garantir um ambiente limpo e saudável.</p>
+                        <h2>Limpeza Predial</h2>
+                        <p>Oferecemos serviços de limpeza e conservação para ambientes comerciais, industriais, e condomínios, com métodos eficazes e produtos de qualidade.</p>
                     </div>
                 </div>
                 <div className="custom-card">
@@ -30,16 +30,16 @@ const Servicos = () => {
                     </div>
                     <div className="custom-content">
                         <h2>Portaria</h2>
-                        <p>Contamos com profissionais qualificados para oferecer serviços de portaria e controle de acesso, garantindo a segurança do seu estabelecimento.</p>
+                        <p>Controle de acesso e organização de entradas e saídas de forma ágil e segura, com porteiros qualificados para garantir a ordem e o bom funcionamento do seu condomínio ou empresa.</p>
                     </div>
                 </div>
                 <div className="custom-card">
                     <div className="img-box">
-                    <img src={require('../../assets/prediall.png')} alt="Manutenção Predial" />
+                    <img src={require('../../assets/recp.png')} alt="Manutenção Predial" />
                     </div>
                     <div className="custom-content">
-                        <h2>Manutenção Predial</h2>
-                        <p>Nossos serviços de manutenção predial incluem reparos elétricos e hidráulicos para manter seu edifício em perfeitas condições.</p>
+                        <h2>Recepção</h2>
+                        <p>Oferecemos um atendimento de qualidade aos seus clientes e visitantes com nossa equipe de recepcionistas, que cuidam da recepção, telefonia e organização de agendas com eficiência e cordialidade.</p>
                     </div>
                 </div>
                 <div className="custom-card">
@@ -47,17 +47,17 @@ const Servicos = () => {
                         <img src={require('../../assets/jardinagem.jpg')} alt="Jardinagem e Paisagismo" />
                     </div>
                     <div className="custom-content">
-                        <h2>Jardinagem e Paisagismo</h2>
-                        <p>Oferecemos manutenção de jardins e áreas verdes para garantir um ambiente agradável e bem-cuidado em sua propriedade.</p>
+                        <h2>Limpeza Hospitalar</h2>
+                        <p>Garantimos um ambiente seguro e livre de contaminações em hospitais e clínicas, seguindo protocolos rigorosos de higiene para a segurança de pacientes e profissionais de saúde.</p>
                     </div>
                 </div>
                 <div className="custom-card">
                     <div className="img-box">
-                        <img src={require('../../assets/admi.png')} alt="Serviços Administrativos" />
+                        <img src={require('../../assets/esp.png')} alt="Serviços Administrativos" />
                     </div>
                     <div className="custom-content">
-                        <h2>Serviços Administrativos</h2>
-                        <p>Contamos com uma equipe de profissionais capacitados para oferecer serviços administrativos, incluindo recepcionistas, digitalização de documentos e apoio logístico.</p>
+                        <h2>Serviços Especializados</h2>
+                        <p>Oferecemos serviços especializados como apoio técnico, assessoria, coordenadoria, manutenção predial e outros serviços personalizados para garantir a excelência no funcionamento da sua empresa.</p>
                     </div>
                 </div>
             </div>
